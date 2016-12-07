270cf0fe04b2cc9f1b802308aad1f62b4c62e0a5
marlonji2360 <marlonji2360@gmail.com>
2016-12-06 19:01:57 -0600

    Proyecto Inicial Caja

---
+++ Caja/Caja/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Caja.Startup))]
+namespace Caja
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
